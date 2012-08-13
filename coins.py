198583feffba8382066908088c4680cd4c66c581
Robin Wilson <robin@rtwilson.com>
2012-08-13 21:35:13 +0100

    Added support for multiple authors - in a fairly nice way (IMHO)

--- coins.py
+++ coins.py
@@ -4,55 +4,53 @@ import urllib
 def parse_authors(authors_string):
 	authors = authors_string.split("and")
 	authors = map(lambda x: x.strip(), authors)
-	print authors
-
 
 	lastname, temp, initials = authors[0].partition(",")
 
-	return (lastname.strip(), initials.strip())
+	other_authors = []
+	for i in range(0, len(authors)):
+		other_lastname, temp, other_initials = authors[i].partition(",")
+		other_authors.append(other_initials.strip() + " " + other_lastname.strip())
+
+	return (lastname.strip(), initials.strip(), other_authors)
 
 entries = bibparse.parse_bib("/Users/robin/Documents/University/MyPublications/AcademicPortfolio/RobinWilson.bib")
 
-i = 0
+for i in range(len(entries)):
+	author1_last, author1_initials, authors = parse_authors(entries[i].data['author'])
 
-print entries[i]
+	data = {'rft.date'  : entries[i].data['year'],
+			'rft.aulast': author1_last,
+			'rft.auinit': author1_initials,
+	}
 
-
-
-
-
-
-author1_last, author1_initials = parse_authors(entries[i].data['author'])
-
-data = {'rft.date'  : entries[i].data['year'],
-		'rft.aulast': author1_last,
-		'rft.auinit': author1_initials,
-		'rft.au'	: author1_initials + " " + author1_last
-}
-
-end = "'></span>"
+	end = "'></span>"
 
 
-if entries[i].btype == "article":
-	# Do a journal article
-	start = "<span class='Z3988' title='url_ver=Z39.88-2004&amp;ctx_ver=Z39.88-2004&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal"
-	data['rft.genre'] = "article"
-	data['rft.atitle'] = entries[i].data['title']
-	data['rft.jtitle'] = entries[i].data['journal']
-else:
-	start = "<span class='Z3988' title='url_ver=Z39.88-2004&amp;ctx_ver=Z39.88-2004&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook"
-	data['rft.genre'] = "proceeding"
-	data['rft.atitle'] =  entries[i].data['title']
-	data['rft.btitle'] = entries[i].data['booktitle']
+	if entries[i].btype == "article":
+		# Do a journal article
+		start = "<span class='Z3988' title='url_ver=Z39.88-2004&amp;ctx_ver=Z39.88-2004&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal"
+		data['rft.genre'] = "article"
+		data['rft.atitle'] = entries[i].data['title']
+		data['rft.jtitle'] = entries[i].data['journal']
+	else:
+		start = "<span class='Z3988' title='url_ver=Z39.88-2004&amp;ctx_ver=Z39.88-2004&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook"
+		data['rft.genre'] = "proceeding"
+		data['rft.atitle'] =  entries[i].data['title']
+		data['rft.btitle'] = entries[i].data['booktitle']
 
 
-s = ""
+	s = ""
 
-for key, value in data.iteritems():
-	s += "&" + key + "=" + value
+	for key, value in data.iteritems():
+		s += "&" + key + "=" + value
 
-s = urllib.quote(s)
-s = s.replace(r"%26", "&amp;")
-s = s.replace(r"%3D", "=")
+	# Do the authors bit as there can be many rft.au tags, which a dict can't cope with
+	for author in authors:
+		s += "&" + 'rft.au' + "=" + author
 
-print start + s + end
+	s = urllib.quote(s)
+	s = s.replace(r"%26", "&amp;")
+	s = s.replace(r"%3D", "=")
+
+	print start + s + end
